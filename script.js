64a068691e14dea693e717469f8de65777561bd9
lifefrequencies <97172386+lifefrequencies@users.noreply.github.com>
2022-01-15 01:08:19 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const navSlide = () => {
+    const burger = document.querySelector(".burger");
+
+    const nav = document.querySelector(".nav-links");
+
+    const navLinks = document.querySelectorAll(".nav-links li");
+
+    /// Toggale map
+
+    burger.addEventListener("click", () => {
+        nav.classList.toggle("nav-active");
+
+        navLinks.forEach((link, index) => {
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5}s`;
+        });
+
+        ///burger animation
+        burger.classList.toggle("toggle");
+    });
+};
+
+navSlide();
